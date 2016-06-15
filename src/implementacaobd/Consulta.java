6777cf4bdf2798eb723ca24176425bfe85e518e3
EltonR <erasch@inf.ufsm.br>
2016-06-15 14:15:44 -0300

    Initial Commit

--- src/implementacaobd/Consulta.java
+++ src/implementacaobd/Consulta.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 
 public class Consulta {
     
+    public static void main(String[] args) {
+        
+    }
+    
     private String consulta;
-    private String[] consulta_v;
+    private String select;
+    private String from;
+    private String where;
     private ArrayList colunas;
     private ArrayList tabelas;
     private ArrayList joins;
@@ -13,40 +19,99 @@ public class Consulta {
     
     public Consulta(String consulta){
         this.consulta = consulta.trim().toUpperCase();
-        consulta_v = this.consulta.split(" ");
+        
         colunas = new ArrayList();
         tabelas = new ArrayList();
         joins = new ArrayList();
-        wheres = new ArrayList();
     }
     
     public String testa(){
         if(consulta.equalsIgnoreCase("")){
             return "Consulta vazia!";
         }
-        if(!consulta_v[0].equals("SELECT"))
+        
+        String[] consulta_v = this.consulta.split(" ");
+        if(!consulta_v[0].equalsIgnoreCase("SELECT"))
             return "Consulta não inicia com SELECT";
-        if(!consulta.contains("FROM"))
+        
+        if(!consulta.contains(" FROM "))
             return "Clausula 'FROM' não encontrada";
-        int i=1;
-        while(!consulta_v[i].equalsIgnoreCase("FROM")){
-            String[] cols=consulta_v[i].split(",");
-            for(int j=0; j<cols.length; j++)
-                colunas.add(cols[j].trim());
-            i++;
+        
+        String[] s0 = consulta.split("SELECT ");
+        String[] s1 = s0[1].split(" FROM ");
+        
+        select = s1[0];
+        
+        if(!s1[1].contains(" WHERE ")){
+            from = s1[1];
+        }else{
+            String[] s2 = s1[1].split(" WHERE ");
+            from = s2[0];
+            where = s2[1];
         }
         
-        while(i<consulta_v.length && !consulta_v[i].equals("WHERE")){
-            tabelas.add(consulta_v[i]);
-            i++;
-        }
-        if(i<consulta_v.length){
-            while(i<consulta_v.length){
-                wheres.add(consulta_v[i]);
-                i++;
+        
+        
+        
+        String s3[] = from.split(",");
+        for(int i=0; i<s3.length; i++){
+            if(!s3[i].contains(" JOIN ")){
+                tabelas.add(s3[i].replace(" ", ""));
+            }else{
+                if(!s3[i].contains(" ON ")){
+                    return "Clausula 'ON' não encontrada";
+                }
+                joins.add(s3[i]);
+                String[] s4 = s3[i].split(" JOIN ");
+                String[] s5 = s4[1].split(" ON ");
+                tabelas.add(s4[0].replace(" ",""));
+                tabelas.add(s5[0].replace(" ", ""));
             }
         }
-        System.out.println(toString());
+        
+        String[] s6 = select.split(",");
+        for(int i=0; i<s6.length; i++){
+            String[] s7 = s6[i].split("[.]");
+            if(s7.length<=1 || s7[1].replace(" ","").equalsIgnoreCase("")){
+                return "Coluna inválida na clausula 'SELECT'";
+            }
+            if(!tabelas.contains(s7[0].replace(" ", ""))){
+                return "Clausula 'SELECT' possui coluna não presente na clausula"
+                        + "'FROM'";
+            }
+            colunas.add(s6[i]);
+        }
+        
+        
+        if(where.contains("(")){
+            int np = 0;
+            for(int i=0; i<where.length(); i++){
+                if(where.substring(i, i+1).equals("(")){
+                    np++;
+                }
+                if(where.substring(i, i+1).equals(")")){
+                    np--;
+                    if(np<0){
+                        return "Erro na utilização do parênteses";
+                    }
+                }
+            }
+            if(np!=0){
+                return "Erro na utilização do parênteses";
+            }
+        }else{
+            if(where.contains(")")){
+                return "Erro na utilização do parênteses";
+            }
+        }
+        
+        for(int i=0; i<tabelas.size(); i++){
+            System.out.println("T="+tabelas.get(i));
+        }
+        for(int i=0; i<colunas.size(); i++){
+            System.out.println("C="+colunas.get(i));
+        }
+        //System.out.println(toString());
         return "OK";
     }
 
