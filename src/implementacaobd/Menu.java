b58a58916031251e54398b3d5aecf4099b7fd19e
EltonR <erasch@inf.ufsm.br>
2016-06-18 15:01:33 -0300

    fghj

--- src/implementacaobd/Menu.java
+++ src/implementacaobd/Menu.java
@@ -10,9 +10,7 @@ public class Menu extends javax.swing.JFrame {
 
     public Menu() {
         initComponents();
-        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setSize(d);
-        jPanel1 = new Painel(1, "");
+        
     }
 
     
@@ -23,6 +21,8 @@ public class Menu extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -34,6 +34,12 @@ public class Menu extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
+
+        jButton2.setText("Ver Arvore Otimizada");
+        jButton2.setEnabled(false);
+
+        jButton3.setText("Ver Arvore Original");
+        jButton3.setEnabled(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -48,6 +54,10 @@ public class Menu extends javax.swing.JFrame {
                         .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 569, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1)))
                 .addContainerGap())
         );
@@ -59,11 +69,15 @@ public class Menu extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addContainerGap(498, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -71,11 +85,15 @@ public class Menu extends javax.swing.JFrame {
         String retorno = c.testa();
         c.geraAlgebraOriginal();
         c.printaArvoreOriginal();
-        if(!retorno.equalsIgnoreCase("OK"))
+        if(!retorno.equalsIgnoreCase("OK")){
             JOptionPane.showMessageDialog(this, retorno, "Erro", JOptionPane.ERROR_MESSAGE);
-        else
+            jButton2.setEnabled(false);
+            jButton3.setEnabled(false);
+        }else{
             System.out.println("OK");
-        
+            jButton2.setEnabled(true);
+            jButton3.setEnabled(true);
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     
@@ -113,6 +131,8 @@ public class Menu extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
